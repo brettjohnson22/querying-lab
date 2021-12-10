9d244b3ad9b5a626f1a619b22c986503d176269d
brettjohnson22 <br.onson@outlook.com>
2021-12-10 17:17:06 -0600

    Added templates and view functions for 1-6

--- querying_lab/school_db/models.py
+++ querying_lab/school_db/models.py
@@ -7,16 +7,25 @@ class Instructor(models.Model):
     last_name = models.CharField(max_length=40)
     hire_date = models.DateField()
 
+    def __str__(self) -> str:
+        return f'{self.first_name} {self.last_name}'
+
 class Student(models.Model):
     first_name = models.CharField(max_length=40)
     last_name = models.CharField(max_length=40)
     year = models.IntegerField(default=9)
     gpa = models.FloatField()
 
+    def __str__(self) -> str:
+        return f'{self.first_name} {self.last_name}'
+
 class Course(models.Model):
     name = models.CharField(max_length=40)
     instructor = models.ForeignKey(Instructor, null=True, blank=True, on_delete=models.SET_NULL)
     credits = models.IntegerField()
+
+    def __str__(self) -> str:
+        return self.name
 
 class StudentCourse(models.Model):
     student = models.ForeignKey(Student, on_delete=models.CASCADE)
@@ -27,3 +36,6 @@ class StudentCourse(models.Model):
         constraints = [
             models.UniqueConstraint(fields=['student', 'course'], name='unique_student_course')
         ]
+
+    def __str__(self) -> str:
+        return f'{self.student} - {self.course}'
--- querying_lab/school_db/views.py
+++ querying_lab/school_db/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from django.db.models import Count
 from .models import Student, Instructor, Course, StudentCourse
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'school/index.html')
 
 def problem_one(request):
     # Find all students who have a GPA greater than 3.0. 
@@ -11,40 +13,63 @@ def problem_one(request):
     context = {
         'students': None
     }
-    return render(request, 'students/index.html', context)
+    return render(request, 'school/one.html', context)
 
 def problem_two(request):
-    # Find all students who have a B+ in any class. 
-    # Order the data by first name alphabetically.
+    # Find all instructors hired prior to 2010
+    # Order by hire date
 
     context = {
-        'students': None
+        'instructors': None
     }
-    return render(request, 'students/index.html', context)
+    return render(request, 'school/two.html', context)
 
 def problem_three(request):
+    # Find all students who have a B+ in any class. 
+    # Order the data by student's first name alphabetically.
+
+    context = {
+        'student_courses': None
+    }
+    return render(request, 'school/three.html', context)
+
+def problem_four(request):
     # Find all students who are taking the Programming class. 
     # Order by their grade. 
 
     context = {
-        'students': None
+        'student_courses': None
     }
-    return render(request, 'students/index.html', context)
+    return render(request, 'school/four.html', context)
 
-def problem_four(request):
+def problem_five(request):
     # Find all students getting an A in the Programming class. 
     # Order by last name.
 
     context = {
-        'students': None
+        'student_courses': None
     }
-    return render(request, 'students/index.html', context)
+    return render(request, 'school/five.html', context)
 
-def problem_five(request):
+def problem_six(request):
     # Find all students with a GPA less than 3.0 who are getting an A in Programming class.
     # Order by GPA.
 
     context = {
-        'students': None
+        'student_courses': None
     }
-    return render(request, 'students/index.html', context)
+    return render(request, 'school/six.html', context)
+
+################## BONUS #################
+# These problems will require using Aggregate functions along with annotate()
+# https://docs.djangoproject.com/en/4.0/topics/db/aggregation/
+# https://docs.djangoproject.com/en/4.0/ref/models/querysets/#annotate
+
+# BONUS ONE
+# Find the instructor who is only teaching one single course
+
+# BONUS TWO
+# Display all students along with the number of credits they are taking
+
+# BONUS THREE
+# Find all students who are getting an A in any course and average their GPAs. Display the number of students and their Average GPA
